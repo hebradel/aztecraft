bab4c3619a2151da4421d7719af50de62260c7bc
hebradel <hebradel@gmail.com>
2025-04-15 18:27:15 +0000

    aztecraft alpha 1.1

--- aztecraft/settings.py
+++ aztecraft/settings.py
@@ -32,6 +32,14 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://aztecraft.onrender.com',
+    'http://127.0.0.1:8000',
+    'http://localhost:8000',
+    'https://localhost:8000',
+    'https://127.0.0.1:8000',
+]
+
 # Application definition
 
 INSTALLED_APPS = [
