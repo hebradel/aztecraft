ca71c6fa54f1eb76c60ddd7e11e0a43ecde3458b
hebradel <hebradel@gmail.com>
2025-05-05 11:55:07 -0500

    Update settings.py 1.1

--- aztecraft/settings.py
+++ aztecraft/settings.py
@@ -33,7 +33,12 @@ ALLOWED_HOSTS = ['*']
 
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://moderate-madeleine-hebradel-7fe6707f.koyeb.app/',
+    'https://aztecraft.onrender.com',
+     'http://127.0.0.1:8000',
+     'http://localhost:8000',
+     'https://localhost:8000',
+     'https://127.0.0.1:8000',
+     'https://moderate-madeleine-hebradel-7fe6707f.koyeb.app/',
 ]
 
 # Application definition
